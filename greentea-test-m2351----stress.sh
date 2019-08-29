bcbb08f9d97ce725b2a563a73da682d474d812fb
Chun-Chieh Li <ccli8@nuvoton.com>
2019-08-29 15:47:15 +0800

    M2351: Rename targets due to max 20 chars limit in online database
    NUMAKER_PFM_M2351_NOPSA_NS  > NU_PFM_M2351_NP_NS
    NUMAKER_PFM_M2351_NOPSA_S   > NU_PFM_M2351_NP_S
    NUMAKER_PFM_M2351_NS        > NU_PFM_M2351_P_NS
    NUMAKER_PFM_M2351_S         > NU_PFM_M2351_P_S

--- greentea-test-m2351----stress.sh
+++ greentea-test-m2351----stress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mbed test -m NUMAKER_PFM_M2351 -t GCC_ARM -n mbed-os-tests-mbed_hal-common_tickers > stress_00.log
+mbed test -m NU_PFM_M2351_NP_NS -t GCC_ARM -n mbed-os-tests-mbed_hal-common_tickers > stress_00.log
 mbedhtrun -p COM16:9600 --skip-flashing -e mbed-os/TESTS/host_tests > stress_01.log
 
 mbedhtrun -p COM16:9600 --skip-flashing -e mbed-os/TESTS/host_tests >> stress_01.log
