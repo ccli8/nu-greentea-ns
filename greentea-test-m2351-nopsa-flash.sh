1c7b2a8f5f8e41576848d91749848204dc83bd84
Chun-Chieh Li <ccli8@nuvoton.com>
2019-09-16 10:16:32 +0800

    M2351: Re-re-rename targets due to max 20 chars limit in online database
    NU_PFM_M2351_NS         > NU_PFM_M2351_NPSA_NS
    NU_PFM_M2351_S          > NU_PFM_M2351_NPSA_S
    NU_PFM_M2351_PSA_NS     > NU_PFM_M2351_NS
    NU_PFM_M2351_PSA_S      > NU_PFM_M2351_S

--- greentea-test-m2351-nopsa-flash.sh
+++ greentea-test-m2351-nopsa-flash.sh
@@ -10,26 +10,26 @@ mbed-os-features-nvstore-tests-nvstore-functionality\
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_S/ARMC6-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_NS -t ARMC6 -n $TEST_LIST > m2351-nopsa_armc6-s_armc6-ns_flash.log
+cp TARGET_IGNORE/NU_PFM_M2351_NPSA_S/ARMC6-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NPSA_NS -t ARMC6 -n $TEST_LIST > m2351-nopsa_armc6-s_armc6-ns_flash.log
 
 # Secure ARMC6/Non-secure GCC_ARM toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_S/ARMC6-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_NS -t GCC_ARM -n $TEST_LIST > m2351-nopsa_armc6-s_gcc-ns_flash.log
+cp TARGET_IGNORE/NU_PFM_M2351_NPSA_S/ARMC6-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NPSA_NS -t GCC_ARM -n $TEST_LIST > m2351-nopsa_armc6-s_gcc-ns_flash.log
 
 # Secure GCC_ARM/Non-secure ARMC6 toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_S/GCC_ARM-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_NS -t ARMC6 -n $TEST_LIST > m2351-nopsa_gcc-s_armc6-ns_flash.log
+cp TARGET_IGNORE/NU_PFM_M2351_NPSA_S/GCC_ARM-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NPSA_NS -t ARMC6 -n $TEST_LIST > m2351-nopsa_gcc-s_armc6-ns_flash.log
 
 # Secure GCC_ARM/Non-secure GCC_ARM toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_S/GCC_ARM-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_NS -t GCC_ARM -n $TEST_LIST > m2351-nopsa_gcc-s_gcc-ns_flash.log
+cp TARGET_IGNORE/NU_PFM_M2351_NPSA_S/GCC_ARM-DEVELOP/NuMaker-mbed-TZ-secure-example/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NPSA_NS -t GCC_ARM -n $TEST_LIST > m2351-nopsa_gcc-s_gcc-ns_flash.log
--- greentea-test-m2351-psa_timer.sh
+++ greentea-test-m2351-psa_timer.sh
@@ -22,26 +22,26 @@ mbed-os-tests-mbedmicro-rtos-mbed-kernel_tick_count\
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_PSA_S/ARMC6-DEVELOP/tfm/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_PSA_NS -t ARMC6 -n $TEST_LIST > m2351-psa_armc6-s_armc6-ns_timer.log
+cp TARGET_IGNORE/NU_PFM_M2351_S/ARMC6-DEVELOP/tfm/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NS -t ARMC6 -n $TEST_LIST > m2351-psa_armc6-s_armc6-ns_timer.log
 
 # Secure ARMC6/Non-secure GCC_ARM toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_PSA_S/ARMC6-DEVELOP/tfm/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_PSA_NS -t GCC_ARM -n $TEST_LIST > m2351-psa_armc6-s_gcc-ns_timer.log
+cp TARGET_IGNORE/NU_PFM_M2351_S/ARMC6-DEVELOP/tfm/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NS -t GCC_ARM -n $TEST_LIST > m2351-psa_armc6-s_gcc-ns_timer.log
 
 # Secure GCC_ARM/Non-secure ARMC6 toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_PSA_S/GCC_ARM-DEVELOP/tfm/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_PSA_NS -t ARMC6 -n $TEST_LIST > m2351-psa_gcc-s_armc6-ns_timer.log
+cp TARGET_IGNORE/NU_PFM_M2351_S/GCC_ARM-DEVELOP/tfm/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NS -t ARMC6 -n $TEST_LIST > m2351-psa_gcc-s_armc6-ns_timer.log
 
 # Secure GCC_ARM/Non-secure GCC_ARM toolchain
 rm -rdf BUILD
 rm -rdf SECURE_IMAGE_STAGE
 mkdir -p SECURE_IMAGE_STAGE
-cp TARGET_IGNORE/NU_PFM_M2351_PSA_S/GCC_ARM-DEVELOP/tfm/* SECURE_IMAGE_STAGE
-mbed test -m NU_PFM_M2351_PSA_NS -t GCC_ARM -n $TEST_LIST > m2351-psa_gcc-s_gcc-ns_timer.log
+cp TARGET_IGNORE/NU_PFM_M2351_S/GCC_ARM-DEVELOP/tfm/* SECURE_IMAGE_STAGE
+mbed test -m NU_PFM_M2351_NS -t GCC_ARM -n $TEST_LIST > m2351-psa_gcc-s_gcc-ns_timer.log
